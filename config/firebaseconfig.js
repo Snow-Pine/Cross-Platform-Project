b73aa97d544b9f357ceed26db2ac4a5817b831e4
shelovesJ <jamesgriffiths23@hotmail.com>
2024-12-13 22:48:22 -0500

    Transferring firebase data to make it work

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -1,28 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-//import necessay services required from firebase
-//Firebase Authentication
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-//Firebase Firestore
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBap0CVEnByHbnnDvcHptmc1vaNU9V53Q8",
-  authDomain: "jeff-project-4282e.firebaseapp.com",
-  projectId: "jeff-project-4282e",
-  storageBucket: "jeff-project-4282e.firebasestorage.app",
-  messagingSenderId: "298376931690",
-  appId: "1:298376931690:web:dfc9e9ccda5b56a0535d13"
+  apiKey: "AIzaSyAgSXTBt4_rxqMO3drnwg_s5BrTXMJr6g0",
+  authDomain: "james-project-cef63.firebaseapp.com",
+  projectId: "james-project-cef63",
+  storageBucket: "james-project-cef63.firebasestorage.app",
+  messagingSenderId: "702520683223",
+  appId: "1:702520683223:web:707bc145e8cf782c7c5e92"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
 
-//instantiate and export firebase service
-//create object of Authentication service and export it
-export const auth = getAuth(app)
-//create object of Firestore service and export it
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
